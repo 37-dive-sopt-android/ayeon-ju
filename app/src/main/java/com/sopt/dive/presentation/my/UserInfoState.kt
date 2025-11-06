cd44714b5c62391b61d58d215511784e575c269f
znayeonzn <wndkdus102@naver.com>
2025-11-06 18:02:24 +0900

    [feat/#4] SAA 적용 & navigation 연결

---
+++ app/src/main/java/com/sopt/dive/presentation/my/UserInfoState.kt
@@ -0,0 +1,8 @@
+package com.sopt.dive.presentation.my
+
+data class UserInfoState(
+    val userId: String = "",
+    val userPassword: String = "",
+    val userNickname: String = "",
+    val userAlcohol: String = ""
+)
