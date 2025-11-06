7ab012203fe1a13c5a10fd0753afe3e8491703e0
znayeonzn <wndkdus102@naver.com>
2025-11-06 18:06:10 +0900

    [delete/#4] isLoggedIn 제거

--- app/src/main/java/com/sopt/dive/data/local/UserLocalDataSource.kt
+++ app/src/main/java/com/sopt/dive/data/local/UserLocalDataSource.kt
@@ -1,22 +1,16 @@
 package com.sopt.dive.data.local
 
-import android.content.Context
 import android.content.SharedPreferences
 import com.sopt.dive.core.util.KeyStorage.IS_LOGGED_IN
 import com.sopt.dive.core.util.KeyStorage.USER_ALCOHOL
 import com.sopt.dive.core.util.KeyStorage.USER_ID
 import com.sopt.dive.core.util.KeyStorage.USER_NICKNAME
 import com.sopt.dive.core.util.KeyStorage.USER_PASSWORD
-import com.sopt.dive.core.util.KeyStorage.USER_PREFS
 import jakarta.inject.Inject
 
 class UserLocalDataSource @Inject constructor(
     private val sharedPreferences: SharedPreferences
-){
-    fun isLoggedIn(): Boolean {
-        return sharedPreferences.getBoolean(IS_LOGGED_IN, false)
-    }
-
+) {
     fun saveUserInfo(
         userId: String,
         userPassword: String,
