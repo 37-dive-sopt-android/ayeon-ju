0788b083ffd4ee7d023f298fc41cfade0f158f84
znayeonzn <wndkdus102@naver.com>
2025-11-11 18:29:15 +0900

    [feat/#4] 심화과제 구현

--- app/src/main/java/com/sopt/dive/presentation/search/SearchRoute.kt
+++ app/src/main/java/com/sopt/dive/presentation/search/SearchRoute.kt
@@ -3,7 +3,9 @@ package com.sopt.dive.presentation.search
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,6 +15,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sopt.dive.R
 import com.sopt.dive.presentation.search.component.ImageCard
+import com.sopt.dive.presentation.search.component.ImageTextCard
 
 @Composable
 fun SearchRoute(
@@ -32,12 +35,17 @@ fun SearchScreen(
         modifier = modifier
             .fillMaxSize()
             .padding(paddingValues = paddingValues),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         ImageCard(
             frontImage = R.drawable.img_front,
             backImage = R.drawable.img_back
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        ImageTextCard(
+            frontImage = R.drawable.img_front
         )
     }
 }
