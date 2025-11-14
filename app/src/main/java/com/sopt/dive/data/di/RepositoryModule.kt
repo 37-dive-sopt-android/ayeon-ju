02c0693655a8afcfbd06c265b266c003d5f8e812
주아연 <142514626+znayeonzn@users.noreply.github.com>
2025-11-14 17:15:33 +0900

    [feat/#6] Repository 모듈 추가

---
+++ app/src/main/java/com/sopt/dive/data/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.sopt.dive.data.di
+
+import com.sopt.dive.data.repositoryimpl.auth.AuthRepositoryImpl
+import com.sopt.dive.domain.repository.auth.AuthRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindAuthRepository(
+        authRepositoryImpl: AuthRepositoryImpl
+    ): AuthRepository
+}
