a2680de175e8015b96410a0f407a6553506c616f
주아연 <142514626+znayeonzn@users.noreply.github.com>
2025-11-14 17:14:37 +0900

    [feat/#6] Service 모듈 추가

---
+++ app/src/main/java/com/sopt/dive/data/di/ServiceModule.kt
@@ -0,0 +1,19 @@
+package com.sopt.dive.data.di
+
+import com.sopt.dive.data.service.auth.AuthService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+    @Provides
+    @Singleton
+    fun providesAuthService(retrofit: Retrofit): AuthService = retrofit.create(
+        AuthService::class.java
+    )
+}
