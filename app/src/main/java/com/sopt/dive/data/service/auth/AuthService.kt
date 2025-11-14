c0317d9397f3af96c2c67336de7fbb1b401f12ff
주아연 <142514626+znayeonzn@users.noreply.github.com>
2025-11-14 23:40:54 +0900

    [feat/#6] SignIn service 작성

--- app/src/main/java/com/sopt/dive/data/service/auth/AuthService.kt
+++ app/src/main/java/com/sopt/dive/data/service/auth/AuthService.kt
@@ -1,7 +1,9 @@
 package com.sopt.dive.data.service.auth
 
+import com.sopt.dive.data.dto.request.auth.SignInRequestDto
 import com.sopt.dive.data.dto.request.auth.SignUpRequestDto
 import com.sopt.dive.data.dto.response.BaseResponse
+import com.sopt.dive.data.dto.response.auth.SignInResponseDto
 import com.sopt.dive.data.dto.response.auth.SignUpResponseDto
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -11,4 +13,9 @@ interface AuthService {
     suspend fun postSignup(
         @Body request: SignUpRequestDto
     ): BaseResponse<SignUpResponseDto>
+
+    @POST("/api/v1/auth/login")
+    suspend fun postSignIn(
+        @Body request: SignInRequestDto
+    ): BaseResponse<SignInResponseDto>
 }
