9000f0ccf3c6b54bc7d8f05d28e70dc6d00e1e2a
주아연 <142514626+znayeonzn@users.noreply.github.com>
2025-11-14 23:43:51 +0900

    [feat/#6] SignIn api 연결

--- app/src/main/java/com/sopt/dive/presentation/signin/SignInViewModel.kt
+++ app/src/main/java/com/sopt/dive/presentation/signin/SignInViewModel.kt
@@ -1,16 +1,62 @@
 package com.sopt.dive.presentation.signin
 
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.sopt.dive.data.local.UserLocalDataSource
+import com.sopt.dive.data.repositoryimpl.auth.AuthRepositoryImpl
+import com.sopt.dive.domain.model.auth.SignInRequestModel
+import com.sopt.dive.domain.repository.auth.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SignInViewModel @Inject constructor(
-    private val userLocalDataSource: UserLocalDataSource
+    private val authRepository: AuthRepository
 ): ViewModel() {
 
-    fun getUserId(): String = userLocalDataSource.getUserId()
+    private val _uiState = MutableStateFlow(SignInState())
+    val uiState : StateFlow<SignInState> = _uiState.asStateFlow()
 
-    fun getUserPassword(): String = userLocalDataSource.getUserPassword()
+    private val _sideEffect = MutableSharedFlow<SignInSideEffect>()
+    val sideEffect = _sideEffect.asSharedFlow()
+
+    fun onUsernameChange(username: String) =
+        _uiState.update { it.copy(username = username) }
+
+    fun onPasswordChange(password: String) =
+        _uiState.update { it.copy(password = password) }
+
+    fun onSignInClick() {
+        with(_uiState.value) {
+            if (username.isBlank() || password.isBlank()) {
+                viewModelScope.launch {
+                    _sideEffect.emit(SignInSideEffect.ShowErrorMessage("nickname 또는 password를 입력해 주세요."))
+                }
+                return
+            }
+
+            viewModelScope.launch {
+                authRepository.postSignIn(
+                    SignInRequestModel(
+                        username = username,
+                        password = password
+                    )
+                ).onSuccess {
+                    _sideEffect.emit(SignInSideEffect.Success)
+                }.onFailure {
+                    _sideEffect.emit(SignInSideEffect.ShowErrorMessage("로그인 실패"))
+                }
+            }
+
+        }
+
+    }
 }
