1784800c127a55157862fbd9505d20afc7804ae1
주아연 <142514626+znayeonzn@users.noreply.github.com>
2025-11-14 23:42:16 +0900

    [feat/#6] SignIn model, mapper 작성

--- app/src/main/java/com/sopt/dive/data/mapper/auth/AuthMapper.kt
+++ app/src/main/java/com/sopt/dive/data/mapper/auth/AuthMapper.kt
@@ -1,6 +1,8 @@
 package com.sopt.dive.data.mapper.auth
 
+import com.sopt.dive.data.dto.request.auth.SignInRequestDto
 import com.sopt.dive.data.dto.request.auth.SignUpRequestDto
+import com.sopt.dive.domain.model.auth.SignInRequestModel
 import com.sopt.dive.domain.model.auth.SignUpRequestModel
 
 fun SignUpRequestModel.toDto() = SignUpRequestDto(
@@ -9,4 +11,9 @@ fun SignUpRequestModel.toDto() = SignUpRequestDto(
     name = this.name,
     email = this.email,
     age = this.age
+)
+
+fun SignInRequestModel.toDto() = SignInRequestDto(
+    username = this.username,
+    password = this.password
 )
