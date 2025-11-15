39cd97f4cbc20b6a00b3da22bd90a41443f5abc9
주아연 <142514626+znayeonzn@users.noreply.github.com>
2025-11-16 02:22:04 +0900

    [feat/#6] [심화과제] DataSource getHomeList 함수 추가

--- app/src/main/java/com/sopt/dive/data/local/UserLocalDataSource.kt
+++ app/src/main/java/com/sopt/dive/data/local/UserLocalDataSource.kt
@@ -5,12 +5,14 @@ import com.sopt.dive.core.util.KeyStorage.USER_ID
 import com.sopt.dive.data.dto.request.auth.SignInRequestDto
 import com.sopt.dive.data.dto.request.auth.SignUpRequestDto
 import com.sopt.dive.data.service.auth.AuthService
+import com.sopt.dive.data.service.home.HomeService
 import com.sopt.dive.data.service.my.MyPageService
 import jakarta.inject.Inject
 
 class UserLocalDataSource @Inject constructor(
     private val authService: AuthService,
     private val myPageService: MyPageService,
+    private val homeService: HomeService,
     private val sharedPreferences: SharedPreferences
 ) {
 
@@ -30,4 +32,6 @@ class UserLocalDataSource @Inject constructor(
     suspend fun postSignIn(request: SignInRequestDto) = authService.postSignIn(request = request)
 
     suspend fun getUserInfo(id: Long) = myPageService.getUserInfo(id)
+
+    suspend fun getHomeListInfo(page: Int, perPage: Int) = homeService.getHomeUserList(page = page, perPage = perPage)
 }
